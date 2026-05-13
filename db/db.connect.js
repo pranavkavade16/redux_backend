889e08d2bb901df87caf888e09682af57fc06c10
pranavkavade16 <pranavkavade69739@gmail.com>
2026-05-13 12:10:10 +0530

    first commit

---
+++ db/db.connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const mongoUri = process.env.MONGODB;
+
+const initializeDatabase = async () => {
+  await mongoose
+    .connect(mongoUri)
+    .then(() => {
+      console.log('Connected to the database.');
+    })
+    .catch((error) => {
+      console.log('Error connecting the database.', error);
+    });
+};
+
+module.exports = {initializeDatabase}
